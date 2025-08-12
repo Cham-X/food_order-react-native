856573f44d208bc92df5a09eb40e0e179146ec77
Shamsudee Zakariyyah <92693681+Cham-X@users.noreply.github.com>
2025-08-12 13:00:22 +0100

    Refactor: Remove unused components and consolidate code structure
    - Deleted HelloWave component and its associated styles.
    - Removed ParallaxScrollView component and its styles.
    - Eliminated ThemedText and ThemedView components to streamline theming.
    - Removed IconSymbol components for iOS and fallback for MaterialIcons.
    - Deleted TabBarBackground components for iOS and Android.
    - Removed Colors constants file to simplify color management.
    - Added mock data for orders and products in assets.
    - Created new components for Collapsible sections and External links.
    - Implemented Haptic feedback for tab navigation.
    - Updated the main layout and tab navigation structure.
    - Added product listing functionality in HomeScreen.
    - Introduced types for Product, Order, and CartItem to enhance type safety.

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,18 @@
+import ProductListItem from '@/src/components/ProductList';
+import { FlatList } from 'react-native';
+import products from "../../../assets/data/products";
+
+
+export default function HomeScreen() {
+  return (
+    <FlatList
+      data={products}
+      renderItem={({ item }) => <ProductListItem product={item} />}
+      numColumns={2}
+      contentContainerStyle={{ gap: 10, padding: 10 }}
+      columnWrapperStyle={{ gap: 10 }}
+    />
+  );
+}
+
+
---
+++ src/components/ProductList.tsx
@@ -0,0 +1,46 @@
+import { Colors } from '@/src/constants/Colors';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { Product } from '../types';
+
+type ProductTypeProps = {
+    product: Product;
+};
+
+const ProductListItem = ({ product }: ProductTypeProps) => {
+    return (
+        <View style={styles.container}>
+            <Image
+                src={product.image || "https://via.placeholder.com/150"}
+                style={styles.image}
+                resizeMode='contain'
+            />
+            <Text style={styles.title}>{product.name}</Text>
+            <Text style={styles.price}>${product.price}</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 10,
+        borderRadius: 10,
+        justifyContent: 'center',
+        backgroundColor: "white",
+        flex: 1,
+        maxWidth: "50%",
+    },
+    title: {
+        fontSize: 18,
+        fontWeight: 600,
+        marginVertical: 10,
+    },
+    price: {
+        fontWeight: "bold",
+        color: Colors.light.tint,
+    },
+    image: {
+        width: "100%",
+        aspectRatio: 1,
+    },
+});
+export default ProductListItem;
